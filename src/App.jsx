39ec2c1805181b02814000cd2c78ae684b29bd46
Adil9212 <abdrazakovadil@gmail.com>
2023-07-14 17:44:00 +0600

    todo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoList from './component/TodoList'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>TODOLIST</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
